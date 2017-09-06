5eaa5ece883b194681b85cd0b89406a21b51cbc6
shehantissera <shehan@duosoftware.com>
2017-09-07 00:22:57 +0530

    added case sentitive things

--- include/authenticity.php
+++ include/authenticity.php
@@ -4,11 +4,11 @@
 $actual_link = "$_SERVER[REQUEST_URI]";
 if(isset($_COOKIE['securityToken'])){
     
-    if($actual_link == "/bitsms/auth/"){
+    if($actual_link == "/bitSMS/auth/"){
         header("Location: ../");
     }
 }else{
-    if($actual_link == "/bitsms/"){
+    if($actual_link == "/bitSMS/"){
         header("Location: auth/");
     }
 }   
